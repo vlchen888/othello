8c06d78d3b57db0eea76cecc05045067c36b023a
Victor Chen <vlchen888@gmail.com>
2018-03-02 00:37:16 -0800

    Implemented minimax, heuristic. Test minimax is correct. Team AI is ``dot''

--- player.cpp
+++ player.cpp
@@ -11,6 +11,10 @@ Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
 
+    board = new Board();
+    // Set colors
+    mySide = side;
+    oppSide = flip(side);
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
@@ -38,9 +42,108 @@ Player::~Player() {
  * return nullptr.
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
-    /*
-     * TODO: Implement how moves your AI should play here. You should first
-     * process the opponent's opponents move before calculating your own move
-     */
-    return nullptr;
+
+    // Process opponent's move by updating board
+    board->doMove(opponentsMove, oppSide);
+    // Find and do my move, and return
+    Move *myMove = getMove();
+    //std::cerr << "myMove: " << myMove << std::endl;
+    board->doMove(myMove, mySide);
+    return myMove;
 }
+
+/**
+ * @brief Using the current board state, choses the next move to make
+ * using a minimax algorithm!
+ */
+Move *Player::getMove() {
+
+    int minimaxDepth;
+    if(testingMinimax) {
+        minimaxDepth = 2;
+    } else {
+        minimaxDepth = 3;
+    }
+    int bestScore = INT_MAX;
+    Move *bestMove = nullptr;
+    // Update with getValidMoves
+    bitset<64> validMoves = board->getValidMoves(mySide);
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
+            if(validMoves[i + 8*j]) {
+                Move *move = new Move(i, j);
+                // Inefficient memory usage again
+                Board *tmp = board->copy();
+                tmp->doMove(move, mySide);
+                int oppScore = minimax(tmp, flip(mySide), minimaxDepth - 1);
+                // Update best move found
+                if(oppScore < bestScore) {
+                    bestScore = oppScore;
+                    delete bestMove;
+                    bestMove = move;
+                } else {
+                    delete move;
+                }
+                delete tmp;
+            }
+        }
+
+    }
+    
+    return bestMove;
+}
+
+/**
+ * @brief The heuristic for the minimax tree
+ */
+int Player::heuristic(Board *curr, Side side) {
+    return curr->count(side) - curr->count(flip(side));
+}
+
+/**
+ * @brief Given a board state and a side, return a heuristic
+ *        representing the strength evaluation of the board state for
+ *        this side.
+ */
+int Player::minimax(Board *curr, Side side, int depthLeft) {
+    // Very inefficient iterative solution
+    if(depthLeft == 0) {
+        return heuristic(curr, side);
+    }
+    int minmax = INT_MAX;
+
+    bitset<64> validMoves = curr->getValidMoves(side);
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
+            if(validMoves[i + 8*j]) {
+                Move move(i, j);
+                Board *tmp = curr->copy();
+                tmp->doMove(&move, side);
+                minmax = min(minmax, minimax(tmp, flip(side), depthLeft - 1));
+                // Very inefficient memory usage
+                delete tmp;
+            }
+        }
+    }
+    // Check if you can pass
+    if(curr->checkMove(nullptr, side)) {
+        minmax = min(minmax, minimax(curr, flip(side), depthLeft - 1));
+    }
+
+    // minmax is the smallest possible gain the adversary can have
+    // so return -minmax for the largest possible gain we can have
+    // (which is the strength of our position)
+    return -1*minmax;
+}
+
+/**
+ * @brief For testing minimax
+ */
+void Player::setBoard(Board *b) {
+    delete board;
+    board = b;
+}
+
+
+
+
--- player.hpp
+++ player.hpp
@@ -4,15 +4,27 @@
 #include <iostream>
 #include "common.hpp"
 #include "board.hpp"
+
+#define flip(side) (side == BLACK ? WHITE : BLACK)
+#define min(a, b) (a < b ? a : b)
+
 using namespace std;
 
 class Player {
-
+private:
+    Board* board;
+    Side mySide;
+    Side oppSide;
 public:
     Player(Side side);
     ~Player();
 
     Move *doMove(Move *opponentsMove, int msLeft);
+    Move *getMove();
+    int heuristic(Board *curr, Side side);
+    int minimax(Board *curr, Side side, int depthLeft);
+
+    void setBoard(Board *b);
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
