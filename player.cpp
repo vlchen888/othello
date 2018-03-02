e7121d740ec4c1a8f5f2c25aa93861005cbe4c69
Victor Chen <vlchen888@gmail.com>
2018-03-01 22:32:58 -0800

    First commit from Victor!

--- player.cpp
+++ player.cpp
@@ -1,5 +1,7 @@
 #include "player.hpp"
 
+// Victor was here!
+
 /*
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
